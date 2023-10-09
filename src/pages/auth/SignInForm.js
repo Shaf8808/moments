d0010bb3ab893f9c4ae720983e5483f71274aa5d
Shaf8808 <shadmankhan08@gmail.com>
2023-10-09 10:55:49 +0000

    Imports current user function

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 import Form from "react-bootstrap/Form";
@@ -14,11 +14,12 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-import { SetCurentUserContext } from "../../App";
+import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
 function SignInForm() {
-  // Use context function, imports callback function setCurentuser from app.js
-  const setCurrentUser = useContext(SetCurentUserContext);
+  // Use context function, imports callback function setCurentuser from
+  // CurrentUserContext.js
+  const setCurrentUser = useSetCurrentUser();
 
   const [signInData, setSignInData] = useState({
     username: "",
